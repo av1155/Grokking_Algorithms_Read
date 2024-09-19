065dc4ee051a0cc686cef85aaa3a0274fffad309
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-19 18:00:54 -0400

    Implemented binarysearch in python

---
+++ Python-Implementations/Chapter1/BinarySearch.py
@@ -0,0 +1,29 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low < high:
+        middle = (low + high) // 2
+        guess = list[middle]
+
+        if guess == item:
+            return middle
+
+        elif guess < item:
+            low = middle + 1
+
+        elif guess > item:
+            high = middle - 1
+
+
+def main():
+    list = [1, 5, 10, 15, 20, 35, 55, 100, 120, 200, 230]
+    item = int(input("Enter the item you want to search for: "))
+    result = binary_search(list, item)
+    if result is not None:
+        print(f"The item you were looking for is at index {result} of the list.")
+    else:
+        print("The item was not found in the list.")
+
+
+main()
