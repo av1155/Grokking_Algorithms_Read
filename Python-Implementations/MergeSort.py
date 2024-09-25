438d848089b82e86f0c55f170ed1383491b72899
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-25 00:08:25 -0400

    Updated comment format

--- Python-Implementations/MergeSort.py
+++ Python-Implementations/MergeSort.py
@@ -1,12 +1,14 @@
 def merge_sort(array):
-    # Recursion to reduce left and right arrays to size 1. When this condition is true,
-    # the left array will complete splitting, and the right array will start.
-    # When the right array also meets the condition, then they will move on to call
-    # the merge function.
+    # Recursion to reduce left and right arrays to size 1.
+    # When this condition is true, the left array will complete
+    # splitting, and the right array will start.
+    # When the right array also meets the condition, then they will move
+    # on to call the merge function.
     if len(array) <= 1:
         return array
 
-    # Split the array in two and create two subarrays for left and right side.
+    # Split the array in two and create two subarrays for left and
+    # right side.
     mid = len(array) // 2
     left = array[:mid]
     right = array[mid:]
@@ -15,19 +17,24 @@ def merge_sort(array):
     left = merge_sort(left)
     right = merge_sort(right)
 
-    # Return the sorted array that the merge function will return with the splitted left and right arrays.
+    # Return the sorted array that the merge function will return
+    # with the splitted left and right arrays.
     return merge(left, right)
 
 
 def merge(left, right):
-    result = []  # Empty array so we can add the sorted elements one by one.
+    # Empty array so we can add the sorted elements one by one.
+    result = []
 
     L = R = 0  # Initialize the indexes for the left and right array.
 
-    # Find the smallest and append it to the results array until L or R indexes make the condition false.
-    # The reasoning is that at some point, left or right will have still one element which has not been
-    # checked yet, but we have nothing else to check it with. This "and" condition allows us to exit
-    # early so that we can simply append whatever is left on that array to the end of the results array.
+    # Find the smallest and append it to the results array until
+    # L or R indexes make the condition false.
+    # The reasoning is that at some point, left or right will have
+    # still one element which has not been checked yet, but we
+    # have nothing else to check it with. This "and" condition allows us
+    # to exit early so that we can simply append whatever is left on
+    # that array to the end of the results array.
     while L < len(left) and R < len(right):
         if left[L] < right[R]:
             result.append(left[L])
@@ -36,7 +43,8 @@ def merge(left, right):
             result.append(right[R])
             R += 1
 
-    # This is where we append whatever was left on that left or right side that we could not compare to anything.
+    # This is where we append whatever was left on that left or right
+    # side that we could not compare to anything.
     result.extend(left[L:])
     result.extend(right[R:])
 
